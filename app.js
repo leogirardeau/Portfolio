be4b7c678054b51973b69b1c1548dd01e95d80ec
leogirardeau <leogir.pro@gmail.com>
2020-08-19 18:44:31 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,39 @@
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".nav-links");
+const links = document.querySelectorAll(".nav-links li");
+
+hamburger.addEventListener("click", () => {
+  navLinks.classList.toggle("open");
+  links.forEach((link) => {
+    link.classList.toggle("fade");
+  });
+});
+
+const texts = [
+  "Bonjour,",
+  "Léo Girardeau,",
+  "Juriste International",
+  "et Européen",
+  "Apprenti Développeur Web.",
+  "Bienvenue !",
+];
+let count = 0;
+let index = 0;
+let currentText = "";
+let letter = "";
+
+//self invoke function
+(function type() {
+  if (count === texts.length) {
+    count = 0;
+  }
+  currentText = texts[count];
+  letter = currentText.slice(0, ++index);
+
+  document.querySelector(".typing").textContent = letter;
+  if (letter.length === currentText.length) {
+    count++;
+    index = 0;
+  }
+  setTimeout(type, 400);
+})();
